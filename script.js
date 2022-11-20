3d7ae86e2c69db90c19751825e13eb29f4533ed2
Vinicius <114451547+ViniciusDeLima18@users.noreply.github.com>
2022-11-20 19:27:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+        strings:[ "Desenvolvedor", "Designer", "Freelancer"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
